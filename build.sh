4938f309c94ff048f568280f56bbf16c310d6726
Ryan Schumacher <Ryan.Schumacher@possible.com>
2016-11-22 18:42:22 -0600

    Updating build script

--- build.sh
+++ build.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
 
-xcodebuild -target 'Good Morning' -scheme 'Good Morning' -configuration Release CONFIGURATION_BUILD_DIR=./bin/
+if [ "$#" -ne 1 ]; then
+    echo "No version number specified"
+    exit
+fi
+
+[ -d "bin/" ] || mkdir "bin/"
+
+
+xcodebuild -target 'Good Morning' -scheme 'Good Morning' -configuration Release CONFIGURATION_BUILD_DIR=./Build/
+
+TAR_NAME="bin/good_morning_$1.tar.gz"
+
+tar cvzf $TAR_NAME "Build/Good Morning" 
+
+if [ "$?" -eq 0 ]; then 
+	echo "Sucessfully generated \"$TAR_NAME\""
+else 
+	echo "Failed to generate \"$TAR_NAME\""
+fi
+
+
 

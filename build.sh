a6c962e485923336037f46493d34042552ca4c59
Ryan Schumacher <Ryan.Schumacher@possible.com>
2016-11-22 21:16:25 -0600

    Updating project settings

--- build.sh
+++ build.sh
@@ -1,23 +1,9 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
-    echo "No version number specified"
-    exit
-fi
-
-[ -d "bin/" ] || mkdir "bin/"
 
 xcodebuild -target 'Good Morning' -scheme 'Good Morning' -configuration Deploy CONFIGURATION_BUILD_DIR=./build/
 
-TAR_NAME="bin/good_morning_$1.tar.gz"
 
-tar cvzf "$TAR_NAME" "Good Morning/" "Good Morning.xcodeproj/project.pbxproj" "Good Morning.xcodeproj/project.xcworkspace/contents.xcworkspacedata"
-
-if [ "$?" -eq 0 ]; then 
-	echo "Sucessfully generated \"$TAR_NAME\""
-else 
-	echo "Failed to generate \"$TAR_NAME\""
-fi
 
 
 
